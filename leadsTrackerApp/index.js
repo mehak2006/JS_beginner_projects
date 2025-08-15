4b3ee38962ff517c6d6b44f334781ddbb39374f4
mehak2006 <69978573+mehak2006@users.noreply.github.com>
2025-08-15 19:04:57 +0530

    delete localStorage code

--- leadsTrackerApp/index.js
+++ leadsTrackerApp/index.js
@@ -11,22 +11,17 @@ const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 const deleteBtn = document.getElementById("delete-btn")
 
-const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
 
-if(leadsFromLocalStorage){
-    myLeads = leadsFromLocalStorage
-    render(myLeads)
-}
+
+
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     inputEl.value = ""
-    localStorage.setItem("myLeads", JSON.stringify(myLeads))
     render(myLeads)
 })
 
 deleteBtn.addEventListener("dblclick", function(){
-    localStorage.clear()
     myLeads = []
     render(myLeads)
 })
