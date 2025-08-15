942a58f5367e6e5d19708b95e2699a1c4191d859
mehak2006 <69978573+mehak2006@users.noreply.github.com>
2025-08-15 19:02:48 +0530

    delete save-tab btn

--- leadsTrackerApp/index.js
+++ leadsTrackerApp/index.js
@@ -1,3 +1,10 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js"
+import { getDatabase } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-database.js"
+const firebaseConfig = {
+    databaseURL : "https://leads-tracker-app-28ce1-default-rtdb.asia-southeast1.firebasedatabase.app/"
+}
+const app = initializeApp(firebaseConfig)
+const database = getDatabase(app)
 let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
@@ -5,7 +12,7 @@ const ulEl = document.getElementById("ul-el")
 const deleteBtn = document.getElementById("delete-btn")
 
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
-const tabBtn = document.getElementById("tab-btn")
+
 
 if(leadsFromLocalStorage){
     myLeads = leadsFromLocalStorage
@@ -24,13 +31,6 @@ deleteBtn.addEventListener("dblclick", function(){
     render(myLeads)
 })
 
-tabBtn.addEventListener("click", function() {
-    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-        myLeads.push(tabs[0].url)
-        localStorage.setItem("myLeads", JSON.stringify(myLeads))
-        render(myLeads)
-    })
-})
 
 function render(leads) {
     let listItems = ""
